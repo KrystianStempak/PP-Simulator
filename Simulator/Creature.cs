bf92bd34bedbdd6742162a1aa25c89cbfc54fba2
KrystianStempak <146976514+KrystianStempak@users.noreply.github.com>
2024-10-29 14:23:03 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+    }
+
+    public string Info => $"{Name} [{Level}]";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I am {Info}!");
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Simulator
+﻿namespace Simulator;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Starting Simulator!\n");
-        }
+        Console.WriteLine("Starting Simulator!\n");
+
+        Creature dragon = new Creature("Dragon", 10);
+        dragon.SayHi();
+
+        Creature unknownCreature = new Creature();
+        unknownCreature.Name = "Unknown";
+        unknownCreature.Level = 1;
+        unknownCreature.SayHi();
+
+        Animals dogs = new Animals { Description = "Dogs", Size = 5 };
+        Console.WriteLine(dogs.Info); 
     }
 }
