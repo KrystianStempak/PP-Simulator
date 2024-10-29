be11990714a1bcb4bb183efd9ddfb39ab1687ce3
KrystianStempak <146976514+KrystianStempak@users.noreply.github.com>
2024-10-29 14:27:30 +0100

    Commit-3

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -16,23 +16,14 @@ public class Creature
         get => _name;
         set
         {
-            if (_name != "Unknown")
-                return;
+            if (_name != "Unknown") return;
 
             value = value.Trim();
-
             if (value.Length > 0 && char.IsLower(value[0]))
                 value = char.ToUpper(value[0]) + value.Substring(1);
-
-            if (value.Length < 3)
-                value = value.PadRight(3, '#');
-
-            if (value.Length > 25)
-                value = value.Substring(0, 25).TrimEnd();
-
-            if (value.Length < 3)
-                value = value.PadRight(3, '#');
-
+            if (value.Length < 3) value = value.PadRight(3, '#');
+            if (value.Length > 25) value = value.Substring(0, 25).TrimEnd();
+            if (value.Length < 3) value = value.PadRight(3, '#');
             _name = value;
         }
     }
@@ -42,9 +33,7 @@ public class Creature
         get => _level;
         set
         {
-            if (_level != 1)
-                return;
-
+            if (_level != 1) return;
             _level = value < 1 ? 1 : (value > 10 ? 10 : value);
         }
     }
@@ -57,14 +46,30 @@ public class Creature
 
     public string Info => $"{Name} [{Level}]";
 
-    public void SayHi()
-    {
-        Console.WriteLine($"Hi, I am {Info}!");
-    }
+    public void SayHi() => Console.WriteLine($"Hi, I am {Info}!");
 
     public void Upgrade()
     {
-        if (Level < 10)
-            _level++;
+        if (Level < 10) _level++;
+    }
+
+    public void Go(Direction direction)
+    {
+        string directionStr = direction.ToString().ToLower();
+        Console.WriteLine($"{Name} goes {directionStr}.");
+    }
+
+    public void Go(Direction[] directions)
+    {
+        foreach (var direction in directions)
+        {
+            Go(direction);
+        }
+    }
+
+    public void Go(string directions)
+    {
+        var parsedDirections = DirectionParser.Parse(directions);
+        Go(parsedDirections);
     }
 }
---
+++ Simulator/DirectionParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public static class DirectionParser
+{
+    public static Direction[] Parse(string input)
+    {
+        List<Direction> directions = new();
+
+        foreach (char c in input.ToUpper())
+        {
+            switch (c)
+            {
+                case 'U':
+                    directions.Add(Direction.Up);
+                    break;
+                case 'R':
+                    directions.Add(Direction.Right);
+                    break;
+                case 'D':
+                    directions.Add(Direction.Down);
+                    break;
+                case 'L':
+                    directions.Add(Direction.Left);
+                    break;
+            }
+        }
+
+        return directions.ToArray();
+    }
+}
